73aece35a0f331d701c2c1ed3768d8810a994a9e
Kacper Burza <65431499+kburza@users.noreply.github.com>
2023-06-12 13:02:28 -0400

    Initial Commit

---
+++ components/contact/contact-area.jsx
@@ -0,0 +1,73 @@
+import ContactForm from "../common/form/contact-form";
+
+
+const contactInfo = [
+  {
+    icon: 'fal fa-map-marker-alt',
+    title: 'Address:',
+    subtitle: '1310 Martley Drive, Mississauga, Ontario, Canada.'
+  },
+  {
+    icon: 'fal fa-envelope-open-text',
+    title: 'Email:',
+    subtitle: 'arashiartdecor@gmail.com'
+  },
+  {
+    icon: 'fal fa-phone-alt',
+    title: 'Phone #:',
+    subtitle: '(416) 994 9291'
+  },
+]
+
+const ContactArea = () => {
+  return (
+    <>
+      <section className="contact__area pb-100 pt-95">
+        <div className="container">
+          <div className="row">
+            <div className="col-xl-6 col-lg-6">
+              <div className="contact__info">
+                <h3>Find us here.</h3>
+                <ul className="mb-55">
+                  {contactInfo.map(item => (
+                    <li key={item.title} className="d-flex mb-35">
+                      <div className="contact__info-icon mr-20">
+                        <i className={item.icon}></i>
+                      </div>
+                      <div className="contact__info-content">
+                        <h6>{item.title}</h6>
+                        <span>{item.subtitle}</span>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+                <p>Reach out to Arashi Furniture and start your journey to owning a bespoke piece of artisan luxury. We're eager to answer your questions, discuss your vision, and create the perfect table or accessory tailored to your preferences. Contact us today and let's bring your unique furniture dreams to life.</p>
+
+                <div className="contact__social">
+                  <ul>
+                  <li><a href="https://www.facebook.com/people/Arashi-Art/pfbid02BVniveuPwKeZpfdgXhZqd4iotSQFa1wkTEUmDWigVy7FhPujzmZ2i2TPQv7vvNJul/" target="_blank"><i className="fab fa-facebook-f"></i></a></li>
+                    <li><a href="https://www.instagram.com/arashiartdecor/" target="_blank"><i className="fab fa-instagram"></i></a></li>
+                    <li><a href="https://twitter.com/arashiartdecor" target="_blank"><i className="fab fa-twitter"></i></a></li>
+                    <li><a href="https://www.tiktok.com/@arashiartdecor" target="_blank"><i className="fab fa-tiktok"></i></a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="col-xl-6 col-lg-6">
+              <div className="contact__form">
+                <h3>Contact Us.</h3>
+                {/* contact form stat */}
+                <ContactForm/>
+                {/* contact form stat */}
+                <p className="ajax-response"></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default ContactArea;
+
---
+++ data/slider-data.js
@@ -0,0 +1,47 @@
+export const home_slider = [
+  {
+    id: 1,
+    bgImg: '/assets/img/slider/slider-1.jpg',
+    title: <>One-of-a-Kind <br /> Luxury Artisan Tables</>,
+    subtitle: <>Experience the blend of luxury and craftsmanship in Arashi Furniture's unique artisan tables, where each piece captures nature's beauty through quality materials and artistry.</>,
+  },
+  {
+    id: 2,
+    bgImg: 'assets/img/slider/slider-2.jpg',
+    title: <>Think Different &<br /> Do it otherwise</>,
+    subtitle: <>Claritas est etiam processus dynamicus, qui sequitur
+      mutationem consuetudium lectorum.</>,
+  },
+  {
+    id: 3,
+    bgImg: 'assets/img/slider/slider-3.jpg',
+    title: <>High Beam<br /> by Tom Chung</>,
+    subtitle: <>High Beam is an adjustable desk or shelf light that
+      offers a wide variety of lighting possibilities.</>,
+  },
+]
+
+export const slider_data_7 = [
+  {
+    id: 1,
+    bgImg: '/assets/img/slider/05/slide111.webp',
+    sm_title:'up to 50% off',
+    meta:true,
+    title: <>Life style that you <br/>need, Yo.</>,
+    subtitle: <>Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum.</>,
+  },
+  {
+    id: 2,
+    bgImg: '/assets/img/slider/05/slide112.webp',
+    sm_title:'Quick parcel delivery',
+    title: <>Creative theme <br/>Awesome.</>,
+    subtitle: <>Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum.</>,
+  },
+  {
+    id: 3,
+    bgImg: '/assets/img/slider/05/slide113.webp',
+    sm_title:'Quick parcel delivery',
+    title: <>The world is<br/>make all.</>,
+    subtitle: <>Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum.</>,
+  },
+]
