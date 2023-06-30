6568b63a0412cada2eb554f41fead6e6671c9308
Kacper Burza <65431499+kburza@users.noreply.github.com>
2023-06-30 12:32:04 -0400

    Contact Form Integration
    - contact us form fully functional with form validation
    - emailjs username, email, msg fields sent to arashi main email

--- components/common/form/contact-form.jsx
+++ components/common/form/contact-form.jsx
@@ -1,112 +1,108 @@
+import React from "react";
 import { useFormik } from "formik";
-import emailjs from "emailjs-com";
-import ErrorMsg from "./error-msg";
-import { contactSchema } from "./validation-schema";
+import * as yup from "yup";
+import emailjs from "@emailjs/browser";
 
-const ContactForm = () => {
-  // contact form
-  const sendEmail = (e) => {
-    e.preventDefault();
+const contactSchema = yup.object().shape({
+  user_name: yup
+    .string()
+    .matches(/^[A-Za-z]+$/, "Name should only contain letters")
+    .required("* Name is required"),
+  user_email: yup
+    .string()
+    .email("* Email is not valid")
+    .required("* Email is required"),
+  message: yup
+    .string()
+    .min(20, "* Message should be at least 20 characters long")
+    .required("* Message is required"),
+});
 
-    emailjs
-      .sendForm(
-        "service_4lf5fqn",
-        "template_i4ktb0z",
-        form.current,
-        "ld7ERP6Oa2NgSfL9S"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-  };
+export const ContactForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      user_name: "",
+      user_email: "",
+      message: "",
+    },
+    initialErrors: {
+      user_name: "* Name should only contain letters",
+      user_email: "* Email is required",
+      message: "* Message is required",
+    },
+    validationSchema: contactSchema,
+    onSubmit: (values, { setSubmitting, resetForm }) => {
+      emailjs
+        .send(
+          "service_4lf5fqn",
+          "template_i4ktb0z",
+          values,
+          "ld7ERP6Oa2NgSfL9S"
+        )
+        .then(
+          (result) => {
+            console.log(result.text);
+            resetForm({});
+            setSubmitting(false);
+            alert(
+              "Thank you for contacting us! We will be in touch with you shortly!"
+            );
+          },
+          (error) => {
+            console.log(error.text);
+            setSubmitting(false);
+          }
+        );
+    },
+  });
 
-  const handleOnSubmit = (values, { resetForm }) => {
-    alert(`${values.name + "\n" + values.email + "\n" + values.msg}`);
-    resetForm();
-  };
-  // use formik
-  const { handleChange, handleSubmit, handleBlur, errors, values, touched } =
-    useFormik({
-      initialValues: { name: "", email: "", msg: "" },
-      validationSchema: contactSchema,
-      onSubmit: handleOnSubmit,
-    });
   return (
-    <>
-      <form onSubmit={sendEmail} id="contact-form">
-        <div className="row">
-          <div className="col-xl-6 col-lg-6">
-            <div className="contact__input">
-              <label>
-                Name <span className="required">*</span>
-              </label>
-              <input
-                id="name"
-                value={values.name}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                type="text"
-              />
-              {touched.name && <ErrorMsg error={errors.name} />}
-            </div>
-          </div>
-          <div className="col-xl-6 col-lg-6">
-            <div className="contact__input">
-              <label>
-                Email <span className="required">*</span>
-              </label>
-              <input
-                id="email"
-                value={values.email}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                type="email"
-              />
-              {touched.email && <ErrorMsg error={errors.email} />}
-            </div>
+    <form onSubmit={formik.handleSubmit}>
+      <div className="row">
+        <div className="col-xl-6 col-lg-6">
+          <div className="contact__input">
+            <label>Name</label>
+            <input
+              type="text"
+              name="user_name"
+              onChange={formik.handleChange}
+              value={formik.values.user_name}
+            />
+            <div style={{ color: "red" }}>{formik.errors.user_name}</div>
           </div>
         </div>
-        <div className="row">
-          <div className="col-xl-12">
-            <div className="contact__input">
-              <label>
-                Subject <span className="required">*</span>
-              </label>
-              <input
-                id="message"
-                value={values.msg}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                type="text"
-              />
-              {touched.msg && <ErrorMsg error={errors.msg} />}
-            </div>
+        <div className="col-xl-6 col-lg-6">
+          <div className="contact__input">
+            <label>Email</label>
+            <input
+              type="email"
+              name="user_email"
+              onChange={formik.handleChange}
+              value={formik.values.user_email}
+            />
+            <div style={{ color: "red" }}>{formik.errors.user_email}</div>
           </div>
         </div>
-        <div className="row">
-          <div className="col-xl-12">
-            <div className="contact__input">
-              <label>Message</label>
-              <textarea cols="30" rows="10"></textarea>
-            </div>
+        <div className="col-xl-12">
+          <div className="contact__input">
+            <label>Message</label>
+            <textarea
+              name="message"
+              onChange={formik.handleChange}
+              value={formik.values.message}
+            />
+            <div style={{ color: "red" }}>{formik.errors.message}</div>
+          </div>
+          <div className="contact__input">
+            <input
+              type="submit"
+              value="Send"
+              disabled={formik.isSubmitting || !formik.isValid || !formik.dirty}
+            />
           </div>
         </div>
-        <div className="row">
-          <div className="col-xl-12">
-            <div className="contact__submit">
-              <button type="submit" className="os-btn os-btn-black">
-                Send Message
-              </button>
-            </div>
-          </div>
-        </div>
-      </form>
-    </>
+      </div>
+    </form>
   );
 };
 
